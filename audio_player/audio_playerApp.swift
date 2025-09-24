ba798f0ff131b7ad596e6d843838f537df2c264a
Katkov Fedor <pweverj126@gmail.com>
2025-09-24 18:35:50 +0300

    First commit ever

---
+++ audio_player/audio_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  audio_playerApp.swift
+//  audio_player
+//
+//  Created by Fedya Katkov on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct audio_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
