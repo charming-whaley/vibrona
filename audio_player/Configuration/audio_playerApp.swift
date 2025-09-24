0182fb430f5cbb808a1de8686173780c640fcb9f
Katkov Fedor <pweverj126@gmail.com>
2025-09-24 18:40:24 +0300

    Updated project structure

--- audio_player/Configuration/audio_playerApp.swift
+++ audio_player/Configuration/audio_playerApp.swift
@@ -1,17 +1,10 @@
-//
-//  audio_playerApp.swift
-//  audio_player
-//
-//  Created by Fedya Katkov on 9/24/25.
-//
-
 import SwiftUI
 
 @main
 struct audio_playerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
     }
 }
---
+++ audio_player/Views/Pages/SettingsView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        NavigationStack {  }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
