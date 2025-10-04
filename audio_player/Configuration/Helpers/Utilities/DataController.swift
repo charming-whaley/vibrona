be3e21ab2c5994fa1185bc6d2bb1772f30186317
Katkov Fedor <pweverj126@gmail.com>
2025-10-04 23:50:40 +0300

    Added DataController

---
+++ audio_player/Configuration/Helpers/Utilities/DataController.swift
@@ -0,0 +1,23 @@
+import Foundation
+import SwiftData
+
+final class DataController {
+    
+    static let shared = DataController()
+    
+    public func retrieveFilteredSongsList() {
+        
+    }
+    
+    public func retrieveFilteredAndSortedSongsList() {
+        
+    }
+    
+    public func retrieveFilteredPlaylistsList() {
+        
+    }
+    
+    public func retrieveFilteredAndSortedPlaylistsList() {
+        
+    }
+}
